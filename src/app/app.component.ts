e2ce57af146dc66f7a9bc8c5fee2f884ebaf2441
tomasArtaza77 <tomasartaza25@gmail.com>
2021-04-06 15:05:29 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini Calculadora';
+  numero1: number=0;
+  numero2: number=0;
+  resultado: number=0;
+
+
+  suma():void{
+    this.resultado = this.numero1+this.numero2;
+  }
+
+  resta():void{
+    this.resultado = this.numero1-this.numero2;
+  }
+  dividir():void{
+    this.resultado = this.numero1/this.numero2;
+  }
+  multiplicar():void{
+    this.resultado = this.numero1*this.numero2;
+  }
+
 }
