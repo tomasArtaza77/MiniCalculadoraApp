b551ae5141206c4023ed44b69749ec288194f9c9
tomasArtaza77 <tomasartaza25@gmail.com>
2021-07-27 15:01:05 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,42 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'calculadora',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mini Calculadora';
+  title = 'Calculadora Pequeña';
+  mensaje = '';
   numero1: number=0;
   numero2: number=0;
+  resultadoParcial: number=0;
   resultado: number=0;
+  bandera = false;
 
 
   suma():void{
-    this.resultado = this.numero1+this.numero2;
+    this.resultadoParcial = this.numero1+this.numero2;
+    
   }
 
   resta():void{
-    this.resultado = this.numero1-this.numero2;
+    this.resultadoParcial = this.numero1-this.numero2;
+    
   }
   dividir():void{
-    this.resultado = this.numero1/this.numero2;
+    this.resultadoParcial = this.numero1/this.numero2;
+    
   }
   multiplicar():void{
-    this.resultado = this.numero1*this.numero2;
+    this.resultadoParcial = this.numero1*this.numero2;
+    
+  }
+
+  igual():void{
+    this.resultado = this.resultadoParcial;
+    this.mensaje = 'Calculo realizado con exito.'
+    this.bandera = true;
   }
 
 }
